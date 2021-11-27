0796317bb95b568f89be819f581b574beaa758a1
CosmicSpectrum <66248422+CosmicSpectrum@users.noreply.github.com>
2021-11-27 16:53:40 +0200

    lib is ready

--- fileStorageLib.js
+++ fileStorageLib.js
@@ -1,38 +1,87 @@
 const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
+const {
+    v4: uuidv4
+} = require('uuid');
 
-module.exports = class s3Managment{
+module.exports = class s3Managment {
     /**
      * Creates an object with the endpoint info
+     * @param {*} accessKeyId The allowed user id
+     * @param {*} secretAccessKey The user secret
+     * @param {*} bucket The target bucket name
      */
-    constructor(accessKeyId,secretAccessKey,bucket){
+    constructor(accessKeyId, secretAccessKey, bucket) {
         this.s3Bucket = new AWS.S3({
             accessKeyId: accessKeyId,
             secretAccessKey: secretAccessKey,
             Bucket: bucket
         })
+        this.bucketName = bucket
     }
 
     /**
      * this function will upload data to the s3 bucket
      * @param {*} file file buffer
      * @param {*} bucketName bucket name as a string
-     * @returns 
+     * @returns Return err or the result of the operation
      */
-    upload(file,bucketName){
-        return new Promise((resolve,reject)=>{
-            const params = {
-                Bucket: bucketName,
-                Key: uuidv4(),
-                Body: file
-            }
-            this.s3Bucket.upload(params,(err,data)=>{
-                if(err)
-                    reject(err)
-                resolve(data)
+    async upload(file) {
+        return new Promise((resolve, reject) => {
+            this.s3Bucket.createBucket(() => {
+                const params = {
+                    Bucket: this.bucketName,
+                    Key: uuidv4(),
+                    Body: file
+                }
+                this.s3Bucket.upload(params, (err, result) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    resolve({
+                        fileKey: params.Key
+                    })
+                })
             })
         })
     }
-    download(){}
-    delete(){}
+
+    /**
+     * This function will return a file buffer as requested
+     * @param {*} Key The object key
+     */
+    download(fileKey) {
+        return new Promise((resolve, reject) => {
+            this.s3Bucket.createBucket(() => {
+                const params = {
+                    Bucket: this.bucketName,
+                    Key: fileKey
+                }
+                this.s3Bucket.getObject(params, (err, data) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    resolve(data);
+                })
+            })
+        })
+    }
+
+    /**
+     * delete an object from the existing bucket.
+     * @param {*} fileKey the file uuid.
+     * @returns the result of the operation
+     */
+    delete(fileKey) {
+        return new Promise((resolve, reject) => {
+            const params = {
+                Bucket: this.bucketName,
+                Key: fileKey
+            }
+            this.s3Bucket.deleteObject(params, (err, result) => {
+                if (err)
+                    console.log(err)
+                resolve(result);
+            })
+        })
+    }
 }
